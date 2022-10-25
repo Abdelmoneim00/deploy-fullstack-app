9f8424fc3a96be94fddad08dec1b6b7185ad94bd
Abdel-Moneim Arabi <ab000arabi22@gmail.com>
2022-10-25 20:52:51 +0200

    try again

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,1 @@
-aws s3 cp --recursive  ./www s3://elasticbeanstalk-us-east-1-735478632316/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://elasticbeanstalk-us-east-1-735478632316/
+aws s3 cp --recursive  ./www s3://udagram-app/
