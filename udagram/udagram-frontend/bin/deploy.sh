f6706f8743a73a9831f7c8b07c9f03fa7ecd0655
Abdel-Moneim Arabi <ab000arabi22@gmail.com>
2022-10-28 06:35:55 +0200

    change the bucket name

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,1 +1,2 @@
-aws s3 cp --recursive  ./www s3://elasticbeanstalk-us-east-1-735478632316/
+aws s3 cp --recursive --acl public-read ./www s3://udagram-deploy/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-deploy/
