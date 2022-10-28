913492a53f14bc5730c288051ff66efa67a08fb1
Abdel-Moneim Arabi <ab000arabi22@gmail.com>
2022-10-28 19:34:28 +0200

    change the deploy command in front-end

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,3 @@
+npm run build
 aws s3 cp --recursive --acl public-read ./www s3://udagram-app-deploy/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-app-deploy/
